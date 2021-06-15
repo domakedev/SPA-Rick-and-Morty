37e33a5e002eeab9482d66434b8546aeb30a71fd
Cesar Guevara Cabrera <sarcansino@gmail.com>
2021-06-14 19:21:01 -0500

    Cambio de EN a ES en HTML

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,8 +13,7 @@ const router= async () => {
     const header = null || document.getElementById('header');//Identificamos en el HTML
     //const content = null || document.getElementById('content');
 
-    
-    header.innerHTML = await Header()()//Insertamos HTML en el HTML pero esperamos que haya render
+    header.innerHTML = await Header()//Insertamos HTML en el HTML pero esperamos que haya render
 
 };
 
